46dc7de01a872962293b57cefcd5c91925f4edfb
Александр Жиженский <zhizhenskij@RIT-L046.rit.va>
2020-03-17 18:05:48 +0300

    added icons and separators in menu

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,12 @@
 import Store from '../../store/';
 import { routeTo } from '../../router/routerUtils';
 
+import {
+    categories,
+    departments,
+    home
+} from '../../icons/';
+
 export default {
     render: async () => {
         const username = Store.state.user.name;
@@ -16,7 +22,7 @@ export default {
                 </div>
                 <div class="dash__header_right_side">
                     <p class="white_p">
-                        <span class="light_p">Привет, ${username || 'Noname'}!</span>
+                        <span class="light_p">${username || 'Noname'}</span>
                         <span class="bold_p logout_menu_btn">Выйти</span>
                     </p>
                 </div>
@@ -24,16 +30,19 @@ export default {
             <div class="dash__main">
                 <div class="dash__left-side">
                     <ul class="dash_menu">
-                        <li class="dash_menu_item"><a class="pure_a nav_menu_link" href="#/">Главная</a></li>
-                        <li class="dash_menu_item"><a class="pure_a nav_menu_link" href="#/categories">Категории</a></li>
-                        <li class="dash_menu_item"><a class="pure_a nav_menu_link" href="#/departments">Отделения</a></li>
+                        <li class="dash_menu_item"><a class="pure_a nav_menu_link" href="#/">${home('menu_icon')} Главная</a></li>
+                        <li class="dash_menu__delimeter">Структура сайта</li>
+                        <li class="dash_menu_item"><a class="pure_a nav_menu_link" href="#/categories">${categories('menu_icon')} Категории</a></li>
+                        <li class="dash_menu_item"><a class="pure_a nav_menu_link" href="#/departments">${departments('menu_icon')} Отделения</a></li>
+                        <li class="dash_menu__delimeter">Наполнение сайта</li>
                         <li class="dash_menu_item">Страницы отделений</li>
                         <li class="dash_menu_item">Документы</li>
                         <li class="dash_menu_item">Учёт и регистрация УМКД</li>
+                        <li class="dash_menu_item">План-календарь</li>
                         <li class="dash_menu_item">Новости</li>
                         <li class="dash_menu_item">Анонсы</li>
+                        <li class="dash_menu__delimeter">Администрирование</li>
                         <li class="dash_menu_item">Пользователи</li>
-                        <li class="dash_menu_item">План-календарь</li>
                         <li class="dash_menu_item">Настройки</li>
                     </ul>
                 </div>
