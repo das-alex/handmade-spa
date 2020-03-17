cede21edb5a9d2a2c79c55acb6891bf50b7841bb
Александр Жиженский <zhizhenskij@RIT-L046.rit.va>
2020-03-17 18:13:02 +0300

    added categories, departments, home icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -31,3 +31,39 @@ export const add = (style) => `
         <rect class="st0" width="32" height="32"/>
     </svg>
 `;
+
+export const categories = (style) => `
+    <svg class="${style}" id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <defs>
+            <style>.cls-1{fill:none;}</style>
+        </defs>
+        <title>categories</title>
+        <path d="M6.76,6l.45.89L7.76,8H12v5H4V6H6.76m.62-2H3A1,1,0,0,0,2,5v9a1,1,0,0,0,1,1H13a1,1,0,0,0,1-1V7a1,1,0,0,0-1-1H9L8.28,4.55A1,1,0,0,0,7.38,4Z" transform="translate(0 0)"/>
+        <path d="M22.76,6l.45.89L23.76,8H28v5H20V6h2.76m.62-2H19a1,1,0,0,0-1,1v9a1,1,0,0,0,1,1H29a1,1,0,0,0,1-1V7a1,1,0,0,0-1-1H25l-.72-1.45a1,1,0,0,0-.9-.55Z" transform="translate(0 0)"/>
+        <path d="M6.76,19l.45.89L7.76,21H12v5H4V19H6.76m.62-2H3a1,1,0,0,0-1,1v9a1,1,0,0,0,1,1H13a1,1,0,0,0,1-1V20a1,1,0,0,0-1-1H9l-.72-1.45a1,1,0,0,0-.9-.55Z" transform="translate(0 0)"/>
+        <path d="M22.76,19l.45.89L23.76,21H28v5H20V19h2.76m.62-2H19a1,1,0,0,0-1,1v9a1,1,0,0,0,1,1H29a1,1,0,0,0,1-1V20a1,1,0,0,0-1-1H25l-.72-1.45a1,1,0,0,0-.9-.55Z" transform="translate(0 0)"/>
+        <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/>
+    </svg>
+`; 
+
+export const departments = (style) => `
+    <svg class="${style}" id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <defs>
+            <style>.cls-1{fill:none;}</style>
+        </defs>
+        <title>tree-view</title>
+        <path d="M30,20V12H22v3H17V7a2,2,0,0,0-2-2H10V2H2v8h8V7h5V25a2,2,0,0,0,2,2h5v3h8V22H22v3H17V17h5v3ZM8,8H4V4H8ZM24,24h4v4H24Zm0-10h4v4H24Z" transform="translate(0)"/>
+        <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/>
+    </svg>
+`;
+
+export const home = (style) => `
+    <svg class="${style}" id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <defs>
+            <style>.cls-1{fill:none;}</style>
+        </defs>
+        <title>home</title>
+        <path d="M16.61,2.21a1,1,0,0,0-1.24,0L1,13.42,2.24,15,4,13.62V26a2,2,0,0,0,2,2H26a2,2,0,0,0,2-2V13.63L29.76,15,31,13.43ZM18,26H14V18h4Zm2,0h0V18a2,2,0,0,0-2-2H14a2,2,0,0,0-2,2v8H6V12.06L16,4.27l10,7.8V26Z" transform="translate(0 0)"/>
+        <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/>
+    </svg>
+`;
